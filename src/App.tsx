eddf0009c609e9fac1b525cf667f9078dceca089
LaurensiusDani <dani.rendragraha@gmail.com>
2022-04-23 08:19:56 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [posts,setPosts]=useState()
+useEffect(()=>{
+  const url='https://swapi.dev/api/people'
+  fetch(url).then(resp=>resp.json())
+  .then(resp=>console.log(resp))
+},[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="center">React App</h1>
+
     </div>
   );
 }
 
 export default App;
+
