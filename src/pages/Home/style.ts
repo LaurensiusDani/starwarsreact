ec38c472e0c05d96a36bdabfb9dceb40b6e533e0
LaurensiusDani <dani.rendragraha@gmail.com>
2022-04-23 09:03:53 +0700

    init

---
+++ src/pages/Home/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const DivContainerHome = styled.div`
+    margin-right: 5%;
+    margin-left: 5%;
+    max-widht: 144px;
+` 
+
+//pakai container, row, search 'container css' di w3schools yang flex container
